36e8997108cbfbd7c69387ee9c4f99d59bdd659b
a-sapon <sapon.anya@gmail.com>
2020-05-14 16:30:28 +0300

    add fonts and Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { Navbar } from './Navbar/Navbar';
+import styles from './App.module.css';
 
 function App() {
-  return <div className='App'>Hello world</div>;
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      <Switch>
+        <Route exact path='/'>
+          {/* Here goes the Home Page */}
+        </Route>
+        <Route path='/test'>{/* Test Description Page */}</Route>
+        <Route path='/question'>{/* Question Page */}</Route>
+        <Route path='/result'>{/* Result Page */}</Route>
+        <Route path='/feedback'>{/* Feedback Page */}</Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
