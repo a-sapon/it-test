68b58c4ee75727f59a1fdadd25ca66e27dbae128
Roman <roman.svitelskyi@gmail.com>
2020-05-21 14:10:58 +0300

    changes in newContactPage

--- src/components/FeedbackPage/ContactCard/ContactCard.js
+++ src/components/FeedbackPage/ContactCard/ContactCard.js
@@ -1,51 +1,27 @@
 import React from "react";
 import "./ContactCard.css";
 import shortid from "shortid";
-const prop = [
-  {
-    photo: "https://i.ibb.co/HdFXj9W/anna.png",
-    name: "Анна Сапон",
-    email: " sapon.anya@gmail.com",
-    speciality: "Front-end Developer",
-    linkedin: "https://www.linkedin.com/in/anna-sapon/",
-    descripiton: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-    id: shortid(),
-  },
-  {
-    photo: "https://i.ibb.co/MBvX1Hb/tania.png",
-    name: " Татьяна Шевцова",
-    email: "tshevtsova.main@gmail.com",
-    speciality: "Front-end Developer",
-    linkedin: "https://www.linkedin.com/in/taniashevtsova/",
-    descripiton: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-    id: shortid(),
-  },
-  {
-    photo: "https://i.ibb.co/dpNVv2c/photo.jpg",
-    name: " Роман Свительский",
-    email: "roman.svitelskyi@gmail.com",
-    speciality: "Front-end Developer",
-    linkedin: "https://www.linkedin.com/in/taniashevtsova/",
-    descripiton: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-    id: shortid(),
-  },
-];
+import arr from "../contact.json";
 
 const ContactCard = () => {
-  const arr = prop;
+  const id = shortid();
   return (
     <ul className="cards">
-      {arr.map(({ name, email, speciality, linkedin, id, photo }) => (
+      {arr.map(({ name, email, speciality, linkedin, photo }) => (
         <li key={id} className="cards-user">
           <div className="cards-user__wrap">
-            <img className="cards-user__img" src={photo} alt="as"></img>
+            <div className="wrap-img">
+              <img className="cards-user__img" src={photo} alt="as"></img>
+            </div>
             <span className="cards-user__name">{name}</span>
             <span className="cards-user__special">{speciality}</span>
-            <span className="cards-user__email">
+            <p className="cards-user__email">
               <b> email: </b>
               <span>{email}</span>
-            </span>
-            <span className="cards-user__linkedin">My linkedin</span>
+            </p>
+            <a className="cards-user__linkedin" href={linkedin} target="_blank">
+              My linkedin
+            </a>
           </div>
         </li>
       ))}
@@ -54,3 +30,4 @@ const ContactCard = () => {
 };
 
 export default ContactCard;
+
